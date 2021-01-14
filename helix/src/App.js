206b77f8b3d78c43a44615e8c61555f59218bea0
divyanshg <divyanshg809@gmail.com>
2021-01-14 17:12:33 +0530

    Final Responsive

--- helix/src/App.js
+++ helix/src/App.js
@@ -1,9 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-   
+   <section class="carousel w-100" aria-label="Gallery">
+        <div class="">
+            <div class="reviews-cont mb-5" style={{display: 'flex !important','overflow-x':'scroll','overflow-y':'hidden'}}>
+                <div class="btn-prev text-center"><i class="fa fa-chevron-left"></i></div>
+                <div class="col-lg-5 col-md-12 col-xs-12 p-4 user-reviews" style={{height:'540px'}}>
+                    <div class="w-100" style={{height:'100%'}}>
+                        <img src="./comma@2x.png" class="comma-bg" alt="" />
+                        <div class="content text-light text-md">
+                            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, quod reprehenderit.
+                                Fugit, qui dolor? Modi, sunt hic. Quia at nam ratione accusamus doloremque culpa quidem
+                                molestiae, explicabo tenetur? Molestias, repellat?</h4>
+                        </div>
+                        <div class="user-sec d-flex">
+                            <img src="./Eleanor-Galtie-black-and-white-circle-headshot.png" alt="" />
+                            <div class="user-name text-light">
+                                <h5>Elizabeth Jones</h5>
+                                <h6 class="font-weight-bold">Carbonix</h6>
+                            </div>
+                        </div>
+                        <img src="./comma@2x.png" class="comma-bg-btm" alt="" />
+                    </div>
+                </div>
+                <div class="col-lg-5 col-md-12 col-xs-12 p-4 user-reviews" style={{height:'540px'}}>
+                    <div class="w-100" style={{height:'100%'}}>
+                        <img src="./comma@2x.png" class="comma-bg" alt="" />
+                        <div class="content text-light text-md">
+                            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, quod reprehenderit.
+                                Fugit, qui dolor? Modi, sunt hic. Quia at nam ratione accusamus doloremque culpa quidem
+                                molestiae, explicabo tenetur? Molestias, repellat?</h4>
+                        </div>
+                        <div class="user-sec d-flex">
+                            <img src="./Eleanor-Galtie-black-and-white-circle-headshot.png" alt="" />
+                            <div class="user-name text-light">
+                                <h5>Elizabeth Jones</h5>
+                                <h6 class="font-weight-bold">Carbonix</h6>
+                            </div>
+                        </div>
+                        <img src="./comma@2x.png" class="comma-bg-btm" alt="" />
+                    </div>
+                </div>
+                <div class="col-lg-5 col-md-12 col-xs-12 p-4 user-reviews" style={{height:'540px'}}>
+                    <div class="w-100" style={{height:'100%'}}>
+                        <img src="./comma@2x.png" class="comma-bg" alt="" />
+                        <div class="content text-light text-md">
+                            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, quod reprehenderit.
+                                Fugit, qui dolor? Modi, sunt hic. Quia at nam ratione accusamus doloremque culpa quidem
+                                molestiae, explicabo tenetur? Molestias, repellat?</h4>
+                        </div>
+                        <div class="user-sec d-flex">
+                            <img src="./Eleanor-Galtie-black-and-white-circle-headshot.png" alt="" />
+                            <div class="user-name text-light">
+                                <h5>Elizabeth Jones</h5>
+                                <h6 class="font-weight-bold">Carbonix</h6>
+                            </div>
+                        </div>
+                        <img src="./comma@2x.png" class="comma-bg-btm" alt="" />
+                    </div>
+                </div>
+                <div class="btn-next text-center"><i class="fa fa-chevron-right"></i></div>
+            </div>
+        </div>
+    </section>
   );
 }
 
